b492407bd65026f88fcffc05f432f8d1d792aad8
serbin70 <serbinin440@gmail.com>
2026-07-27 15:35:07 +0200

    build: remove debug signing from Android release candidate

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -15,10 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.walkingrpg.walking_rpg_mobile"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -27,9 +24,9 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // CI produces an unsigned release-candidate AAB. Store signing is configured
+            // only in a protected publishing environment and is never replaced by debug keys.
+            signingConfig = null
         }
     }
 }
